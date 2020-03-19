71e230587ab8ff47b6620b4c877ec11bdba0cef6
Jedrzej Paulus <jmtpaulus@gmail.com>
2020-03-19 09:01:46 +0100

    some excercises to understand the function expression

--- playground/custom-functions/ways-to-make-a-function-FINISHED.js
+++ playground/custom-functions/ways-to-make-a-function-FINISHED.js
@@ -1,6 +1,16 @@
 console.log('Ways to make a function works works');
 
 // annonymous function
-function (firstName){
-    return `Good day Dr.${nfirstName}`;
-}
+// function (firstName){
+//     return `Good day Dr. ${firstName}`;
+// }
+
+// function expression - putting a function into variable
+
+const doctorize = function(firstName) {
+  return `Hi Dr. ${firstName}`;
+};
+
+const summaryOfNumbers = function(addNumbers) {
+  return `The sum of this operation is ${addNumbers}`;
+};
