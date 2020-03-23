5415cd211a66e118d9b9cd69832646e61485a7af
Jedrzej Paulus <jmtpaulus@gmail.com>
2020-03-23 09:36:36 +0100

    Making arrow functions training

--- playground/custom-functions/ways-to-make-a-function-FINISHED.js
+++ playground/custom-functions/ways-to-make-a-function-FINISHED.js
@@ -17,7 +17,31 @@ const summaryOfNumbers = function(addNumbers) {
 
 // Arrow function - shorter function, concise syntax
 
-function inchToCm(inches){
+function inchToCm(inches){ // regular function
   const cm = inches * 2.54;
   return cm;
 }
+
+function inchtoCm2(inches) {
+  return inches * 2.54;
+}
+/* eslint-disable */
+
+const cmToInch = (centimeter) => {
+  return centimeter / 2.54;
+}
+
+// how to make an arrow function easier
+
+const cmToInch2 = (centimeter) => centimeter / 2.54;
+
+// if there is only one parameter, we can remove the parenthesis
+
+const cmtoInch3 = centimeter => centimeter / 2.54;
+
+function add( a, b = 3){
+  const total = a + b;
+  return total;
+}
+// this block can be turned to an arrow function
+const adding = (a, b = 3) => a + b;
