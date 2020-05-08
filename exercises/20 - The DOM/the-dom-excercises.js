d256eeac45cb954d9eb47ad2d3ce19eb9305414a
Jedrzej Paulus <jmtpaulus@gmail.com>
2020-05-08 09:04:15 +0200

    Learning to get elements

--- exercises/20 - The DOM/the-dom-excercises.js
+++ exercises/20 - The DOM/the-dom-excercises.js
@@ -1,12 +1,15 @@
-const paragraph = document.querySelector('p');
-console.log(paragraph);
+// const paragraph = document.querySelector('p');
+// console.log(paragraph);
 
-const image = document.querySelector('img');
-console.log(image);
+// const image = document.querySelector('img');
+// console.log(image);
 
-const changeText = document.getElementsByClassName('parag');
-console.log(changeText);
-changeText.textContent = 'This is text that was changed in the console'; // changed in console
+// const changeText = document.getElementsByClassName('parag');
+// console.log(changeText);
+// changeText.textContent = 'This is text that was changed in the console'; // changed in console
 
-const newText = document.querySelector('p')
-newText.textContent = 'This is a text that I changed';
+function replaceText() {
+var str = document.getElementById('para')
+var new = str.replace('I', 'He');
+document.getElementsByClassName.innerHTML = new
+};
