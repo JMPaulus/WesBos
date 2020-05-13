58a910d337d2dc554337f5eb839168a56e8f21d7
Jedrzej Paulus <jmtpaulus@gmail.com>
2020-05-13 08:53:38 +0200

    How to create elements in the browser's memory

---
+++ exercises/20 - The DOM/creating.js
@@ -0,0 +1,6 @@
+console.log('Creating is OK, plugged in');
+
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'I am a P like paragraph';
+newParagraph.classList.add('special');
+console.log(newParagraph);
