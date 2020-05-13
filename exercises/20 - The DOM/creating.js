2c52547efb14635cc729c9024cb63f36db8d1920
Jedrzej Paulus <jmtpaulus@gmail.com>
2020-05-13 09:06:14 +0200

    How to grab an element in DOM and put it inside an element created in JS

--- exercises/20 - The DOM/creating.js
+++ exercises/20 - The DOM/creating.js
@@ -4,3 +4,17 @@ const newParagraph = document.createElement('p');
 newParagraph.textContent = 'I am a P like paragraph';
 newParagraph.classList.add('special');
 console.log(newParagraph);
+
+const imageToAdd = document.createElement('img');
+imageToAdd.src = 'https://picsum.photos/500';
+imageToAdd.alt = 'Image added from JS';
+console.log(imageToAdd);
+
+const myDiv = document.createElement('div');
+myDiv.classList.add('wrapper');
+console.log(myDiv);
+
+//how to add an element to the DOM
+
+document.body.appendChild(newParagraph); //grab an element from the JS file and put it to the element in DOM
+document.body.appendChild(imageToAdd);
