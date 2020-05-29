7f740f2d4865a6baf66172cdda0e4f1e52ba4a31
Jedrzej Paulus <jmtpaulus@gmail.com>
2020-05-29 08:42:26 +0200

    traversing and removing nodes

--- exercises/20 - The DOM/traversing.js
+++ exercises/20 - The DOM/traversing.js
@@ -3,3 +3,14 @@ console.log('It works');
 const JJ = document.querySelector('.JJ');
 console.log(JJ.children); // element
 console.log(JJ.childNodes); // node
+console.log(JJ.firstElementChild);
+console.log(JJ.lastElementChild);
+console.log(JJ.previousElementSibling);
+console.log(JJ.nextElementSibling);
+console.log(JJ.parentElement); 
+
+const paragraph = document.createElement('p'); //create element
+paragraph.textContent = 'This is what I will remove'; // add a text content to the element
+document.body.appendChild(paragraph); // added to html
+
+paragraph.remove(); // created and removed using JS
